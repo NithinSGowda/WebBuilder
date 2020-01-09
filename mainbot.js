b1ccbad0c80e7df60c3edbc5de8ee7cfaa380c2b
Nithin.S <nithin.s491@gmail.com>
2020-01-09 22:56:48 +0530

    Add files via upload

--- mainbot.js
+++ mainbot.js
@@ -18,6 +18,11 @@ activateChildren(document);
 console.log(childrenactivated);
 alert("The website is ready to edit. Happy editing :)");
 
+const allLinks=document.querySelectorAll("a");
+for(linki of allLinks){
+  linki.href="javascript:void(0);";
+}
+
 
 const elements=document.querySelectorAll('.et');
 
@@ -81,6 +86,7 @@ for(element of elements)
 function dragElement(elmnt) {
   
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  elmnt.style.zIndex = "10" ;
   if (document.getElementById(elmnt.id)) {
     /* if present, the header is where you move the DIV from:*/
     document.getElementById(elmnt.id).onmousedown = dragMouseDown;
@@ -149,45 +155,14 @@ function nxt3(){
 
 
 function titleCase(str) {
-  // Step 1. Lowercase the string
   str = str.toLowerCase();
-  // str = "I'm a little tea pot".toLowerCase();
-  // str = "i'm a little tea pot";
-  
-  // Step 2. Split the string into an array of strings
   str = str.split(' ');
-  // str = "i'm a little tea pot".split(' ');
-  // str = ["i'm", "a", "little", "tea", "pot"];
-  
-  // Step 3. Create the FOR loop
   for (var i = 0; i < str.length; i++) {
     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-  /* Here str.length = 5
-    1st iteration: str[0] = str[0].charAt(0).toUpperCase() + str[0].slice(1);
-                   str[0] = "i'm".charAt(0).toUpperCase()  + "i'm".slice(1);
-                   str[0] = "I"                            + "'m";
-                   str[0] = "I'm";
-    2nd iteration: str[1] = str[1].charAt(0).toUpperCase() + str[1].slice(1);
-                   str[1] = "a".charAt(0).toUpperCase()    + "a".slice(1);
-                   str[1] = "A"                            + "";
-                   str[1] = "A";
-    3rd iteration: str[2] = str[2].charAt(0).toUpperCase()   + str[2].slice(1);
-                   str[2] = "little".charAt(0).toUpperCase() + "little".slice(1);
-                   str[2] = "L"                              + "ittle";
-                   str[2] = "Little";
-    4th iteration: str[3] = str[3].charAt(0).toUpperCase() + str[3].slice(1);
-                   str[3] = "tea".charAt(0).toUpperCase()  + "tea".slice(1);
-                   str[3] = "T"                            + "ea";
-                   str[3] = "Tea";
-    5th iteration: str[4] = str[4].charAt(0).toUpperCase() + str[4].slice(1);
-                   str[4] = "pot".charAt(0).toUpperCase() + "pot".slice(1);
-                   str[4] = "P"                           + "ot";
-                   str[4] = "Pot";                                                         
-    End of the FOR Loop*/
   }
   
   
-  return str.join(' '); // ["I'm", "A", "Little", "Tea", "Pot"].join(' ') => "I'm A Little Tea Pot"
+  return str.join(' '); 
 }
 
 
@@ -206,8 +181,12 @@ const smallBox = document.getElementsByClassName('c');
 		const recognition = new SpeechRecognition();
 		recognition.interimResults = true;
 		
-		let p= document.createElement('p');
-		const words = document.querySelector('.words');
+    let p= document.createElement('p');
+    let wordssss=document.createElement('div');
+    document.body.appendChild(wordssss);
+    wordssss.classList.add('words');
+    wordssss.style.display='none';
+    const words = document.querySelector('.words');
 		words.appendChild(p);
 	
 		recognition.addEventListener('result', e => {
