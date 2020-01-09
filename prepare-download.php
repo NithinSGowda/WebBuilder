8aa6d1ab13b809817331e751347dff98a15455ba
Nithin.S <nithin.s491@gmail.com>
2020-01-09 07:17:30 +0530

    Update prepare-download.php

--- prepare-download.php
+++ prepare-download.php
@@ -3,7 +3,7 @@
     echo "Got the url as $websiteName";
     exec("wget -r ".$websiteName." -P WEBSITES 2>&1",$op,$ret); 
     //chmod("WEBSITES/.$websiteName",777);
-    //chmod("WEBSITES/.$websiteName./index.html",777);
+    chmod("WEBSITES/.$websiteName./index.html",777);
     $script="<script defer src=\"mainbot.js\"></script>";
     exec("cp mainbot.js WEBSITES/$websiteName");
     $handle = fopen("WEBSITES/".$websiteName."/index.html", 'a');
